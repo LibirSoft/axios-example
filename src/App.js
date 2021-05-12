0fefe78a7328a4fd17598cfab03e025df113ae62
LibirSoft <libir@outlook.com>
2021-05-13 02:46:47 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    users: [],
+    isloading:true
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((data) => data.data)
+        .then((users) => {
+          this.setState({
+            users: users,
+            isloading: false
+
+          });
+        });
+    },3000)
+
+  }
+
+  render() {
+    const { isloading } = this.state;
+    
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {isloading ? "Loading...": ""}
+        {this.state.users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default App;
